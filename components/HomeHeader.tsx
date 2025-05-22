bde8dbaf500d98d55a755c431c14ad53160d1a33
sonkiee <annagu.kennedy@gmail.com>
2025-05-22 15:00:12 +0000

    May 22, 2025, 4:00 PM

--- components/HomeHeader.tsx
+++ components/HomeHeader.tsx
@@ -17,7 +17,9 @@ const HomeHeader = () => {
           }}
         >
           <AntDesign name="questioncircleo" size={24} color="black" />
-          <View></View>
+          <View>
+            <Feather name="user" color={"green"} size={24} />
+          </View>
         </View>
       </View>
       <Text>HomeHeader</Text>
@@ -30,12 +32,17 @@ export default HomeHeader;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "white",
+    // paddingTop: 20,
+    // paddingHorizontal: 20,
+    // shadowColor: "#000",
+    // shadowOpacity: 0.2,
   },
   actionContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    // paddingHorizontal: 20,
+    paddingHorizontal: 10,
     // paddingVertical: 10,
   },
 });
