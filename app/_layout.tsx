c2d61cfa5c11afb059e9e36137c223323c677779
sonkiee <annagu.kennedy@gmail.com>
2025-05-22 11:05:29 +0000

    May 22, 2025, 12:05 PM

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <Stack />
+      <StatusBar style="dark" />
+    </>
+  );
 }
