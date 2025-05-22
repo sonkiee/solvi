550a0d3314f399eb00690b47757aab95803e32c1
sonkiee <annagu.kennedy@gmail.com>
2025-05-22 16:08:12 +0000

    May 22, 2025, 5:08 PM

---
+++ components/CardsSlider.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const lists = [];
+
+const CardsSlider = () => {
+  return (
+    <View>
+      <Text>CardsSlider</Text>
+    </View>
+  );
+};
+
+export default CardsSlider;
+
+const styles = StyleSheet.create({});
