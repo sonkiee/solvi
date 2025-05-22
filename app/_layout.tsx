ce81d35de60274d35412bcd73799a0bcd098b5e7
sonkiee <annagu.kennedy@gmail.com>
2025-05-22 13:30:13 +0000

    May 22, 2025, 2:30 PM

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,16 @@
+import Container from "@/components/ui/Container";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
-    <>
+    <Container>
       <Stack
         screenOptions={{
           headerShown: false,
         }}
       />
       <StatusBar translucent backgroundColor="transparent" style="auto" />
-    </>
+    </Container>
   );
 }
