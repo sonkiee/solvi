8572d2ef269b5f3fed6904001ef74b41c1e49eb5
sonkiee <annagu.kennedy@gmail.com>
2025-05-22 16:16:42 +0000

    May 22, 2025, 5:16 PM

--- components/CardsSlider.tsx
+++ components/CardsSlider.tsx
@@ -6,31 +6,37 @@ const lists = [
     label: "monthly revenue ",
     content: "¥ 23,000",
     icon: "wallet-outline",
+    colors: ["#ccc", "#ccc", "#ccc"],
   },
   {
     label: "active orders ",
     content: "12",
     icon: "wallet-outline",
+    colors: ["#ccc", "#ccc", "#ccc"],
   },
   {
     label: "unlock limits",
     content: "get business account",
     icon: "wallet-outline",
+    colors: ["#ccc", "#ccc", "#ccc"],
   },
   {
     label: "new users",
     content: "",
     icon: "wallet-outline",
+    colors: ["#ccc", "#ccc", "#ccc"],
   },
   {
     label: "sms sent",
     content: "100",
     icon: "wallet-outline",
+    colors: ["#ccc", "#ccc", "#ccc"],
   },
   {
     label: "refer & earn",
     content: "get ¥20 bonus",
     icon: "wallet-outline",
+    colors: ["#ccc", "#ccc", "#ccc"],
   },
 ];
 
