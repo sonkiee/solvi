d2a9bd844910dc93b798bc4e325a5e3cbe898199
sonkiee <annagu.kennedy@gmail.com>
2025-05-22 16:09:57 +0000

    May 22, 2025, 5:09 PM

--- components/CardsSlider.tsx
+++ components/CardsSlider.tsx
@@ -1,7 +1,38 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-const lists = [];
+const lists = [
+  {
+    label: "Monly ",
+    content: "",
+    icon: "wallet-outline",
+  },
+  {
+    label: "Monly ",
+    content: "",
+    icon: "wallet-outline",
+  },
+  {
+    label: "Monly ",
+    content: "",
+    icon: "wallet-outline",
+  },
+  {
+    label: "Monly ",
+    content: "",
+    icon: "wallet-outline",
+  },
+  {
+    label: "Monly ",
+    content: "",
+    icon: "wallet-outline",
+  },
+  {
+    label: "refer & earn ",
+    content: "",
+    icon: "wallet-outline",
+  },
+];
 
 const CardsSlider = () => {
   return (
