dccbc3bad3cd8e19ced66ed07e6349a7c296166b
sonkiee <annagu.kennedy@gmail.com>
2025-05-22 14:17:45 +0000

    May 22, 2025, 3:17 PM

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -64,6 +64,7 @@ export default function TabsLayout() {
             backgroundColor: "transparent",
           },
           // headerTransparent: true,
+          headerShown: false,
         }}
       >
         <Tabs.Screen
