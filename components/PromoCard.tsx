fe7e80d112ce26594893c00adc6c320469175ba5
sonkiee <annagu.kennedy@gmail.com>
2025-05-22 16:21:01 +0000

    May 22, 2025, 5:21 PM

---
+++ components/PromoCard.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const PromoCard = () => {
+  return (
+    <View style={styles.container}>
+      <Text>PromoCard</Text>
+    </View>
+  );
+};
+
+export default PromoCard;
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 8,
+  },
+});
