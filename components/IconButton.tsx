96959bc8f7b270224d0f46b3956236a765aee756
sonkiee <annagu.kennedy@gmail.com>
2025-05-22 15:23:23 +0000

    May 22, 2025, 4:23 PM

---
+++ components/IconButton.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Pressable, StyleSheet, Text } from "react-native";
+
+const IconButton = ({
+  title = "button",
+  icon,
+}: {
+  title: string;
+  icon?: React.ReactElement;
+}) => {
+  return (
+    <Pressable style={styles.container}>
+      <Text>{title}</Text>
+    </Pressable>
+  );
+};
+
+export default IconButton;
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 6,
+  },
+});
