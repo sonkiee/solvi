a71149011ea6d5e0da3532abc93f64ea584a39dd
sonkiee <annagu.kennedy@gmail.com>
2025-05-22 14:55:35 +0000

    May 22, 2025, 3:55 PM

--- components/HomeHeader.tsx
+++ components/HomeHeader.tsx
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 const HomeHeader = () => {
   return (
     <View style={styles.container}>
+      <View style={styles.actionContainer}></View>
       <Text>HomeHeader</Text>
     </View>
   );
@@ -14,5 +15,12 @@ export default HomeHeader;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  actionContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    // paddingHorizontal: 20,
+    // paddingVertical: 10,
   },
 });
