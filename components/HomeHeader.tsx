12364ea1e39bd524968cc9db7a7d1d660774aa65
sonkiee <annagu.kennedy@gmail.com>
2025-05-22 14:58:27 +0000

    May 22, 2025, 3:58 PM

--- components/HomeHeader.tsx
+++ components/HomeHeader.tsx
@@ -1,10 +1,25 @@
+import { AntDesign, Feather } from "@expo/vector-icons";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 const HomeHeader = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.actionContainer}></View>
+      <View style={styles.actionContainer}>
+        <Feather name="bell" size={24} color="black" />
+
+        <View
+          style={{
+            flexDirection: "row",
+            gap: 10,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <AntDesign name="questioncircleo" size={24} color="black" />
+          <View></View>
+        </View>
+      </View>
       <Text>HomeHeader</Text>
     </View>
   );
