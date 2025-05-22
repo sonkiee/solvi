58fdcbee630856839aa4455bb47ff190a13a59cd
sonkiee <annagu.kennedy@gmail.com>
2025-05-22 11:46:18 +0000

    May 22, 2025, 12:46 PM

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -3,8 +3,16 @@ import {
   Feather,
   FontAwesome,
   MaterialCommunityIcons,
-} from "@expo/vector-icons/";
+} from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+import { StyleSheet, View } from "react-native";
+
+// Fix: Rename to PascalCase
+const ExchangeTabIcon = ({ color, size }: { color: string; size: number }) => (
+  <View style={styles.exchange}>
+    <FontAwesome name="yen" color={color} size={size} />
+  </View>
+);
 
 export default function TabsLayout() {
   return (
@@ -21,7 +29,7 @@ export default function TabsLayout() {
         name="bills"
         options={{
           tabBarIcon: ({ color, size }) => (
-            <AntDesign name="filetext1" scolor={color} size={size} />
+            <AntDesign name="filetext1" color={color} size={size} />
           ),
         }}
       />
@@ -29,7 +37,7 @@ export default function TabsLayout() {
         name="exchange"
         options={{
           tabBarIcon: ({ color, size }) => (
-            <FontAwesome name="yen" color={color} size={size} />
+            <ExchangeTabIcon color={color} size={size} />
           ),
         }}
       />
@@ -52,3 +60,16 @@ export default function TabsLayout() {
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  exchange: {
+    // position: 'absolute',
+    // top: 15,
+    // right: 15,
+    // backgroundColor: 'white',
+    // borderRadius: 50,
+    // padding: 10,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+  },
+});
