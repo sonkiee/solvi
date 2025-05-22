9fb63d91c0050d40a72a2eb31b9760d552700cfe
sonkiee <annagu.kennedy@gmail.com>
2025-05-22 13:52:12 +0000

    May 22, 2025, 2:52 PM

--- app/(app)/(tabs)/home.tsx
+++ app/(app)/(tabs)/home.tsx
@@ -1,7 +1,12 @@
+import Container from "@/components/ui/Container";
 import { StyleSheet, Text } from "react-native";
 
 const HomeScreen = () => {
-  return <Text>HomeScreen</Text>;
+  return (
+    <Container>
+      <Text>HomeScreen</Text>
+    </Container>
+  );
 };
 
 export default HomeScreen;
