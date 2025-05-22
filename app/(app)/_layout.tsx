5ecaf18c16d798566037d9863c091aae79a98b07
sonkiee <annagu.kennedy@gmail.com>
2025-05-22 11:08:48 +0000

    May 22, 2025, 12:08 PM

---
+++ app/(app)/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+
+const AppLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="home" />
+    </Stack>
+  );
+};
+
+export default AppLayout;
---
+++ app/(app)/home.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Welcome to the Home Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const style = StyleSheet.create({});
