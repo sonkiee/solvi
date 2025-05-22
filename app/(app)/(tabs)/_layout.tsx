a949332c394b36fe5b0ef32d5c2ca76178090af7
sonkiee <annagu.kennedy@gmail.com>
2025-05-22 12:36:21 +0000

    May 22, 2025, 1:36 PM

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -57,7 +57,6 @@ export default function TabsLayout() {
         },
         tabBarBackground: GradientTabBarBackground,
         tabBarInactiveTintColor: "#eeeeee",
-
         tabBarActiveTintColor: "#ffffff",
       }}
     >
