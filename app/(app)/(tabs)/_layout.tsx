3286371afb2d06898c9345410bd5f186ca8fa4b7
sonkiee <annagu.kennedy@gmail.com>
2025-05-22 11:27:30 +0000

    May 22, 2025, 12:27 PM

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -5,6 +5,7 @@ export default function TabsLayout() {
     <Tabs>
       <Tabs.Screen name="home" />
       <Tabs.Screen name="bills" />
+      <Tabs.Screen name="exchange" />
       <Tabs.Screen name="products" />
       <Tabs.Screen name="profile" />
     </Tabs>
---
+++ app/(app)/(tabs)/exchange.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const ExchangeScreen = () => {
+  return (
+    <View>
+      <Text>ExchangeScreen</Text>
+    </View>
+  );
+};
+
+export default ExchangeScreen;
+
+const styles = StyleSheet.create({});
