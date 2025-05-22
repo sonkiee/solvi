6f4d7d80d7e7986a7abca07921486f08e1435058
sonkiee <annagu.kennedy@gmail.com>
2025-05-22 13:29:30 +0000

    May 22, 2025, 2:29 PM

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,18 +1,15 @@
-import Container from "@/components/ui/Container";
 import { Stack } from "expo-router";
 
 const AppLayout = () => {
   return (
-    <Container>
-      <Stack>
-        <Stack.Screen
-          name="(tabs)"
-          options={{
-            headerShown: false,
-          }}
-        />
-      </Stack>
-    </Container>
+    <Stack>
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
   );
 };
 
