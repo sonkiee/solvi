c2533da9cd0fd8901649227c93ac18c22ba6a114
sonkiee <annagu.kennedy@gmail.com>
2025-05-22 11:29:11 +0000

    May 22, 2025, 12:29 PM

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -1,9 +1,17 @@
+import Feather from "@expo/vector-icons/Feather";
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="home" />
+      <Tabs.Screen
+        name="home"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="home" color={color} size={size} />
+          ),
+        }}
+      />
       <Tabs.Screen name="bills" />
       <Tabs.Screen name="exchange" />
       <Tabs.Screen name="products" />
