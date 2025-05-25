a39481ef06a3b9bdfa7c627eee53297409ab9be1
sonkiee <annagu.kennedy@gmail.com>
2025-05-25 12:08:12 +0000

    May 25, 2025, 1:08 PM

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,12 @@
 // import { Redirect } from "expo-router";
 import { Redirect } from "expo-router";
+import { useState } from "react";
 
 export default function Index() {
+  const [user, setUser] = useState(null);
+
+  if (!user) {
+    return <Redirect href="/(auth)/sign-in" />;
+  }
   return <Redirect href="/home" />;
 }
