ba0ca8ed3c4e8fc52ac9d4a3dea1953a3aa3b962
sonkiee <annagu.kennedy@gmail.com>
2025-05-25 14:54:29 +0000

    May 25, 2025, 3:54 PM

--- app/(app)/transfer.tsx
+++ app/(app)/transfer.tsx
@@ -4,50 +4,62 @@ import Container from "@/components/ui/Container";
 import TextInput from "@/components/ui/Input";
 import { currency } from "@/utils/currency";
 import { Feather } from "@expo/vector-icons";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 const TransferScreen = () => {
   return (
     <Container top={1}>
       <HeaderBack title={"Tranfer Funds"} />
-      <ScrollView
-        style={styles.container}
-        contentInsetAdjustmentBehavior="automatic"
-        keyboardDismissMode="on-drag"
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+        style={{ flex: 1 }}
       >
-        <View>
-          <Containing>
-            <Text style={styles.text}>Available Balance:</Text>
-            <Text style={styles.amountText}>{currency(10000)}</Text>
-            {/* Transfer form */}
-          </Containing>
+        <ScrollView
+          style={styles.container}
+          contentInsetAdjustmentBehavior="automatic"
+          keyboardShouldPersistTaps="handled"
+        >
+          <View>
+            <Containing>
+              <Text style={styles.text}>Available Balance:</Text>
+              <Text style={styles.amountText}>{currency(10000)}</Text>
+              {/* Transfer form */}
+            </Containing>
 
-          <View style={styles.content}>
-            <Text style={styles.label}> Transfer Deatils</Text>
+            <View style={styles.content}>
+              <Text style={styles.label}> Transfer Deatils</Text>
 
-            <TextInput
-              title="Recipient"
-              placeholder="Enter recipient's name or ID"
-              style={{
-                backgroundColor: "#fff",
-              }}
-              label={{ color: "#333" }}
-              container={{ backgroundColor: "#fff" }}
-              icon={<Feather name="user" size={19} color="#aaa" />}
-            />
+              <TextInput
+                title="Recipient"
+                placeholder="Enter recipient's name or ID"
+                style={{
+                  backgroundColor: "#fff",
+                }}
+                label={{ color: "#333" }}
+                container={{ backgroundColor: "#fff" }}
+                icon={<Feather name="user" size={19} color="#aaa" />}
+              />
 
-            <TextInput
-              title="Amount"
-              placeholder="Enter amount"
-              container={{ backgroundColor: "#fff" }}
-              label={{ color: "#333" }}
-              icon={<Feather name="credit-card" size={18} color="#aaa" />}
-            />
+              <TextInput
+                title="Amount"
+                placeholder="Enter amount"
+                container={{ backgroundColor: "#fff" }}
+                label={{ color: "#333" }}
+                icon={<Feather name="credit-card" size={18} color="#aaa" />}
+              />
 
-            <Button title="Transfer funds" gradient />
+              <Button title="Transfer funds" gradient />
+            </View>
           </View>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </Container>
   );
 };
