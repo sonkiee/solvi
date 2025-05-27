318ba0eb59cb2b86decbc2459f4f3273db723b06
sonkiee <annagu.kennedy@gmail.com>
2025-05-27 10:37:15 +0000

    May 27, 2025, 11:37 AM

---
+++ app/(app)/(tabs)/(profile)/personal-details.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const PersonalDetailsScreen = () => {
+  return (
+    <View>
+      <Text>PersonaldetailsScreen</Text>
+    </View>
+  );
+};
+
+export default PersonalDetailsScreen;
+
+const styles = StyleSheet.create({});
